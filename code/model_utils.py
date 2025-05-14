803989244cf42bdfe0bdb75a5cae3f1869bf66ab
Yves Greatti <ygrepo@gmail.com>
2025-05-14 11:51:32 -0400

    updates

--- code/model_utils.py
+++ code/model_utils.py
@@ -26,7 +26,6 @@ class StoreItemDataset(Dataset):
         return self.X[idx], self.y[idx], self.w[idx]
 
 
-# 2) Define the Shallow NN with tanh activations
 class ShallowNN(nn.Module):
     def __init__(self, input_dim, hidden_dim=64, output_dim=None):
         super().__init__()
@@ -35,6 +34,7 @@ class ShallowNN(nn.Module):
             nn.Linear(input_dim, hidden_dim),
             nn.Tanh(),
             nn.Linear(hidden_dim, output_dim),
+            # nn.Hardtanh(min_val=0.0, max_val=1.0),  # clips to [0,1]
             nn.Sigmoid(),  # outputs in (0,1)
         )
 
@@ -42,7 +42,6 @@ class ShallowNN(nn.Module):
         return self.net(x)
 
 
-# 3) Implement NWRMSLE loss
 class NWRMSLELoss(nn.Module):
     def __init__(self):
         super().__init__()
