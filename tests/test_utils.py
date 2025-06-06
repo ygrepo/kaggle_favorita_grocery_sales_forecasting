0676ffd2f228ed687309c01534fbbddc2ba7ba00
Yves Greatti <yvgrotti@gmail.com>
2025-06-06 11:11:45 -0400

    Fix imports in tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from datetime import datetime
 import numpy as np
-from src.utils import (     
-    generate_sales_features,
+from src.utils_1 import (
+    generate_nonoverlap_window_features,
     generate_cyclical_features,
     add_next_window_targets,
 )
