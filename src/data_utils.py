9d22fe81691b476c15802cfb2602b8a5a41484c7
Yves Greatti <ygrepo@gmail.com>
2025-07-23 09:44:18 -0400

    Fixing X y features

--- src/data_utils.py
+++ src/data_utils.py
@@ -866,14 +866,14 @@ def create_y_targets_from_shift(
     df = df.sort_values(["store_item", "start_date"]).reset_index(drop=True)
 
     logger.info(f"Feature prefixes: {feature_prefixes}")
-    y_target_dfs = add_y_targets_from_shift(df, window_size, feature_prefixes)
+    y_target_dfs = list(add_y_targets_from_shift(df, window_size, feature_prefixes))
     if not y_target_dfs:
         logger.warning(
             "No valid y targets were generated. Check your input data and window size."
         )
-        return pd.DataFrame()  # or raise an exception or return empty DataFrame
+        return pd.DataFrame()
     df = pd.concat(y_target_dfs, ignore_index=True)
-    df.sort_values(["store_item", "start_date"]).reset_index(drop=True)
+    df = df.sort_values(["store_item", "start_date"]).reset_index(drop=True)
 
     return df
 
