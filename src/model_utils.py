ad6247d619c5b858c2c3ee0fa16611278be281ff
Yves Greatti <ygrepo@gmail.com>
2025-08-05 18:04:00 -0400

    Updated traing scripts

--- src/model_utils.py
+++ src/model_utils.py
@@ -1454,6 +1454,7 @@ def train_model_unified(
 
     # Save history
     if history_dir:
+        logger.info(f"Saving history to {history_dir}")x
         history_fn = history_dir / f"{model_name}.csv"
         history = safe_append_to_history(history_fn, history)
 
