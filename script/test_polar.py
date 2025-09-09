106e5c1bc58e2563dec844aec819d6534914fe91
Yves Greatti <ygrepo@gmail.com>
2025-09-09 11:28:21 -0400

    Added Polars

--- script/test_polar.py
+++ script/test_polar.py
@@ -1,19 +1,31 @@
-import polars as pl
-import numpy as np
+import polars as pl, numpy as np, rmm, cudf_polars, pylibcudf
 
+print("polars:", pl.__version__)
+print("cudf_polars:", cudf_polars.__version__)
+print("pylibcudf:", pylibcudf.__version__)
+
+# optional: pre-allocate a GPU memory pool (helps perf)
+rmm.reinitialize(pool_allocator=True, initial_pool_size="36GB")
+
+N = 300_000
 df = pl.DataFrame(
     {
-        "store": np.repeat([1, 2, 3], 1_000_00),
-        "item": np.tile(np.arange(1000), 300),
-        "date": np.tile(np.arange(300), 1000),
-        "unit_sales": np.random.rand(300_000).astype(np.float32),
+        "store": np.repeat([1, 2, 3], N // 3),
+        "item": np.tile(np.arange(1000), N // 1000),
+        "date": np.tile(np.arange(300), N // 300),
+        "unit_sales": np.random.rand(N).astype("float32"),
     }
 )
 
 # CPU
-df.lazy().group_by(["store", "item", "date"]).agg(pl.col("unit_sales").sum()).collect()
+_ = (
+    df.lazy()
+    .group_by(["store", "item", "date"])
+    .agg(pl.col("unit_sales").sum())
+    .collect()
+)
 
-# GPU
+# GPU (watch nvidia-smi for VRAM usage)
 out = (
     df.lazy()
     .group_by(["store", "item", "date"])
