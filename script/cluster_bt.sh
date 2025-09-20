8e12f359abda84bb6ad82577b238e2ad79357b94
Yves Greatti <ygrepo@gmail.com>
2025-09-20 13:13:26 -0400

    Tuning clustering

--- script/cluster_bt.sh
+++ script/cluster_bt.sh
@@ -32,8 +32,8 @@ MAX_PVE_DROP="0.01"
 MIN_SIL="-0.05"  
 MIN_KEEP="6"  
 TOP_K="10"
-K_ROW=0
-K_COL=0
+K_ROW=3
+K_COL=3
 
 TOP_RANK_FN="${DATA_GROWTH_RATE_DIR}/growth_rate_2014_January_top_53_store_2000_item_cluster_bt_top_rank.csv"
 SUMMARY_FN="${DATA_GROWTH_RATE_DIR}/growth_rate_2014_January_top_53_store_2000_item_cluster_bt_summary.csv"
