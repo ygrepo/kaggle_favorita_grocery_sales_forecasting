d583c85719c284e2c6a14d460a91689f7d9a1cc8
Yves Greatti <ygrepo@gmail.com>
2025-09-20 14:49:19 -0400

    Tuning clustering

--- src/BTNMF_util.py
+++ src/BTNMF_util.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Dict, Any, Iterable, Optional, Callable
+from typing import Dict, Any, Iterable, Optional, Callable, Tuple
 import pandas as pd
 from sklearn.cluster import KMeans
 from src.BinaryTriFactorizationEstimator import BinaryTriFactorizationEstimator
