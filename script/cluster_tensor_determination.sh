56d751f93233be4dfc6617350f8bcbf96a01945c
Yves Greatti <ygrepo@gmail.com>
2025-11-08 08:26:58 -0500

    Fixing plots

--- script/cluster_tensor_determination.sh
+++ script/cluster_tensor_determination.sh
@@ -10,11 +10,12 @@ echo "Project root: $PROJECT_ROOT"
 cd "$PROJECT_ROOT"
 
 # Default configuration
-METHOD="ntf"
+METHOD="tucker"
 DATA_DIR="${PROJECT_ROOT}/output/data"
 OUTPUT_DATA_DIR="${PROJECT_ROOT}/output/data"
 DATA_FN="${OUTPUT_DATA_DIR}/2014_January_top_53_store_2000_item_growth_rate_imputed_features.parquet"
-MODEL_FN="${OUTPUT_DATA_DIR}/20251004_${METHOD}_2014_January_top_53_store_2000_item_growth_rate_clusters.pt"
+DATE=$(date +"%Y%m%d")
+MODEL_FN="${OUTPUT_DATA_DIR}/${DATE}_${METHOD}_2014_January_top_53_store_2000_item_growth_rate_clusters.pt"
 
 #RANKS="40,300,7"
 RANKS="150"
