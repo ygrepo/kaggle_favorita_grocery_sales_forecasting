636b5bf0c999ed08a15083c6b7c8d3f969810e77
Yves Greatti <ygrepo@gmail.com>
2025-12-05 12:13:21 -0500

    Exp

--- src/compare_time_series_models.py
+++ src/compare_time_series_models.py
@@ -99,9 +99,9 @@ def main():
         "dl_past_covs": Path(
             "output/metrics/20251204_2013_2014_store_2000_item_cyc_features_all_dl_past_covs_metrics.csv"
         ),
-        # "dl_future_covs": Path(
-        #     "output/metrics/20251201_2013_2014_store_2000_item_cyc_features_dl_future_covs_metrics.csv"
-        # ),
+        "tcn_future_covs": Path(
+            "output/metrics/20251205_2013_2014_store_2000_item_cyc_features_tcn_all_dl_future_covs_metrics.csv"
+        ),
         "dl_past_future_covs": Path(
             "output/metrics/20251204_2013_2014_store_2000_item_cyc_features_all_dl_past_future_covs_metrics.csv"
         ),
